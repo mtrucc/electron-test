bd19735d55774b975d68cb5bb4e9fe26c5e105d2
mtrucc <mtrucc@qq.com>
2021-08-13 13:56:45 +0800

    element-ui样例

--- renderer.js
+++ renderer.js
@@ -4,3 +4,15 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const inputElement = document.getElementById("input");
+inputElement.addEventListener("change", handleFiles, false);
+function handleFiles() {
+  const fileList = this.files; /* now you can work with the file list */
+  console.log('fileList :>> ', fileList);
+}
+
+const webview = document.querySelector('webview')
+webview.addEventListener('dom-ready', () => {
+  webview.openDevTools()
+})
